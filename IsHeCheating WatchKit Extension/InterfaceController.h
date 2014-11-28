544292dfd923db3c1cf518bf196bbe6c470649a9
Stas Shulgin <stasshulgin@Mac-mini-Stas.local>
2014-11-28 22:28:16 +0200

    Include watch ui and extension in the project

---
+++ IsHeCheating WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  IsHeCheating WatchKit Extension
+//
+//  Created by Stas Shulgin on 28.11.14.
+//  Copyright (c) 2014 Tinkerz. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
