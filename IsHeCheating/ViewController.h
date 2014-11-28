a8401eb1e2266c3bab64a20b405660cd95a4cc90
Stas Shulgin <stasshulgin@Mac-mini-Stas.local>
2014-11-28 20:48:19 +0200

    Initial Commit

---
+++ IsHeCheating/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IsHeCheating
+//
+//  Created by Stas Shulgin on 28.11.14.
+//  Copyright (c) 2014 Tinkerz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
